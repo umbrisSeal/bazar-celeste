4c34033079f806f6a1d4656e612e2df98e2542c6
umbrisSeal <156217584+umbrisSeal@users.noreply.github.com>
2026-06-25 13:47:17 -0600

    Added header banner.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,46 @@
 
 function App() {
+    const fechaActual = new Date();
+    const meses = [
+        "enero",
+        "febrero",
+        "marzo",
+        "abril",
+        "mayo",
+        "junio",
+        "julio",
+        "agosto",
+        "septiembre",
+        "octubre",
+        "noviembre",
+        "diciembre"
+    ];
+
+    const añoActual = fechaActual.getFullYear();
+    const mesActual = meses[fechaActual.getMonth()];
+
     return <>
-        <p> Hola mundo </p>
         {/* Header fijo */}
-        <header></header>
+        <header>
+            <div>
+                <p> Titulo </p>
+            </div>
+            <div className="bg-header-banner text-white py-2 px-6 font-semibold text-xs tracking-wider">
+                <p> {`Catálogo Completo — Actualizado ${mesActual} ${añoActual}`.toUpperCase()} </p>
+            </div>
+        </header>
         {/* Catalogo de Productos */}
-        <main></main>
+        <main>
+            <p> Catalogo </p>
+        </main>
         {/* Metodos de Pago y de Entrega */}
-        <section></section>
+        <section>
+            <p> Metodos de Pago </p>
+        </section>
         {/* Footer con contacto */}
-        <footer></footer>
+        <footer>
+            <p> Footer </p>
+        </footer>
     </>
 }
 
