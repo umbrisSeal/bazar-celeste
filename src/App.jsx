c17c56a053e5e2641aa17ab929a4c3fb81603320
umbrisSeal <156217584+umbrisSeal@users.noreply.github.com>
2026-06-26 22:06:41 -0600

    Fixed responsive design.

--- src/App.jsx
+++ src/App.jsx
@@ -134,14 +134,14 @@ function App() {
 
         {/* Header fijo */}
         <header>
-            <div className="flex justify-between items-center py-4 px-6 bg-header text-header">
+            <div className="flex flex-col lg:flex-row gap-4 lg:gap-0 justify-between items-center py-4 px-6 bg-header text-header">
                 {/* Left */}
-                <div className="flex items-center gap-4">
+                <div className="flex flex-col lg:flex-row items-center gap-4">
                     <div className="bg-orange-500 text-white w-12 h-12 rounded-xl flex items-center justify-center">
                         <ShoppingBag />
                     </div>
                     <div>
-                        <h1 className="font-black text-4xl text-white select-none"> El Bazar de <span className="text-yellow-300"> Celeste </span> </h1>
+                        <h1 className="font-black text-4xl text-white select-none text-center lg:text-left"> El Bazar de <span className="text-yellow-300"> Celeste </span> </h1>
                         <h2 className="uppercase text-sm tracking-wider font-semibold"> Tarjetas · Electrónica · Coleccionables · Y Más </h2>
                     </div>
                 </div>
@@ -157,7 +157,7 @@ function App() {
                 </div>
             </div>
 
-            <div className="bg-orange-500 text-white py-2 px-6 font-semibold text-sm tracking-wider">
+            <div className="bg-orange-500 text-white py-2 px-6 font-semibold text-sm tracking-wider text-center lg:text-left">
                 <p className="uppercase"> {`Catálogo Completo — Actualizado ${mesActual} ${añoActual}`} </p>
             </div>
         </header>
@@ -166,7 +166,7 @@ function App() {
         {/* Catalogo de Productos */}
         <main className="p-6 flex flex-col justify-center items-center min-h-100">
             {/* Vista normal, catalogo */}
-            <div className={`flex gap-8 flex-wrap w-full ${cargando || errorCatalogo ? 'hidden' : ''}`}>
+            <div className={`flex gap-8 flex-wrap justify-center w-full ${cargando || errorCatalogo ? 'hidden' : ''}`}>
                 {
                     catalogo
                     .filter((producto) => producto.inventario > 0)
@@ -202,9 +202,9 @@ function App() {
 
         {/* Metodos de Pago y de Entrega */}
         <section className="text-emerald-900 px-6 py-4 gap-1">
-            <div>
+            <div className="">
                 <h3 className="uppercase font-bold text-2xl text-center text-white"> Metodos de Pago </h3>
-                <div className="flex gap-12 justify-center py-6 text-xl font-semibold">
+                <div className="flex flex-wrap gap-8 lg:gap-12 justify-center py-6 text-xl font-semibold">
                     <div className="flex flex-col items-center w-50 text-center gap-2">
                         <Landmark size={60} />
                         <p> Transferencia Bancaria </p>
@@ -244,7 +244,7 @@ function App() {
 
 
         {/* Footer con contacto */}
-        <footer className="bg-header text-header text-center py-8 uppercase text-sm tracking-wider font-semibold">
+        <footer className="bg-header text-header text-center py-8 uppercase text-xs lg:text-sm tracking-wider font-semibold">
             <p> &copy; El bazar de celeste · Todos los derechos reservados </p>
         </footer>
 
